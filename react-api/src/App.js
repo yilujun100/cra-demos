52ac13d322135d54874a7487516eb059c5d10316
yilujun <yilujun@sefonsoft.com>
2021-01-29 00:14:02 +0800

    feat: add RefDemo

---
+++ react-api/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Routes from './routes';
+import './App.css';
+function App() {
+    return (
+        <div className="app">
+            <Router>
+                <aside className="aside-menu">
+                    <ul>
+                        {Routes.map(route => (
+                            <li>
+                                <Link to={route.path}>{route.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </aside>
+                <section className="content">
+                    <Switch>
+                        {Routes.map(route => (
+                            <Route path={route.path} component={route.component} />
+                        ))}
+                    </Switch>
+                </section>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
---
+++ react-api/src/routes.js
@@ -0,0 +1,10 @@
+import RefDemo from './demos/ref';
+
+const routes = [
+    {
+        path: '/ref',
+        component: RefDemo,
+        title: 'RefDemo'
+    }
+];
+export default routes;
